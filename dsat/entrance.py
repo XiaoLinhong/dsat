b0488bc530d2df35ebab5f4e6a4141daa77a409b
liangqian <liangqian@3clear.com>
2020-06-05 15:20:12 +0800

    新增：重复提交下载任务

--- dsat/entrance.py
+++ dsat/entrance.py
@@ -4,13 +4,22 @@ from .configure import load_cfg
 
 from . import download
 
+from . import handler
+
 def main():
     ''' 主程序 '''
     # 加载配置
     cfg = load_cfg()
 
     # 下载卫星数据
-    getattr(download, cfg.satellite).download(cfg, cfg.thisTime, cfg.products[cfg.satellite])
+    #print(cfg.thisTime)
+    #for i in range(20):
+    flag = getattr(download, cfg.satellite).download(cfg, cfg.thisTime, cfg.products[cfg.satellite])
+    #if flag: break
+    while not flag:
+           print('Resubmit the download task')
+           flag = getattr(download, cfg.satellite).download(cfg, cfg.thisTime, cfg.products[cfg.satellite])
+       
 
     # 处理数据
-   
+    # getattr(handler, cfg.satellite).handler(cfg, cfg.thisTime, cfg.products[cfg.satellite]) 
